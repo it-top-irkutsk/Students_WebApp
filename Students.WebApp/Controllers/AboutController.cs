b282dfa6b8f8463ff9deae95fdaafaad3668309f
Starinin Andrey <starininandrey@gmail.com>
2022-03-07 16:34:16 +0300

    Init

---
+++ Students.WebApp/Controllers/AboutController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Students.WebApp.Controllers;
+
+public class AboutController : Controller
+{
+    // GET
+    public IActionResult Index(string id)
+    {
+        if (string.IsNullOrEmpty(id))
+        {
+            return View("Index");
+        }
+        if (int.TryParse(id, out var Id))
+        {
+            return View("Index", Id.ToString());
+        }
+
+        return BadRequest("Неверный запрос");
+    }
+}
